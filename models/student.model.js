03b400acb080fb12a2d3e9b1e9b98230a3d0dd23
ash160 <ashish160kumar@gmail.com>
2021-04-01 16:56:55 +0530

    more features

---
+++ models/student.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+var studentSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    city: {
+        type: String
+    }
+});
+
+
+mongoose.model('Student', studentSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Compiler' });
 });
 
+router.get('/pricing', function(req, res, next) {
+  res.render('pricing');
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 router.get('/api',function(req,res){
-  res.send("Compiler API  will come soon ...............")
+  res.send("Compiler API  will come soon ..............")
 });
 
 
